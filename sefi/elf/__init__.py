dcd83df48dcbb8b6cb0193b6fc8f81ddfdeac48c
anthony cantor <ar.cantor@gmail.com>
2013-09-09 21:11:44 -0600

    added license

--- sefi/elf/__init__.py
+++ sefi/elf/__init__.py
@@ -1,3 +1,18 @@
+# Copyright 2013 anthony cantor
+# This file is part of sefi.
+# 
+# sefi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#  
+# sefi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with sefi.  If not, see <http://www.gnu.org/licenses/>.
 import sys
 
 from elftools.elf.elffile import ELFFile
